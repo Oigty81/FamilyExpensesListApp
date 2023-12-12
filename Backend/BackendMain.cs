90feb06e7857775a6aadd4bf5b1244f17c5c361a
Henry Voigt <oigty69@freenet.de>
2023-12-12 22:20:38 +0100

    added backend main class

---
+++ Backend/BackendMain.cs
@@ -0,0 +1,25 @@
+﻿namespace Backend
+{
+    using Backend.Utilities;
+    using Backend.Webservice;
+
+    public class BackendMain
+    {
+        public static IWebserviceHost? WebServiceHostInstance { get; private set; }
+
+        public static ISystemUtilities? SystemUtilitiesInstance { get; private set; }
+
+        public static IResourceLoader? RessourceLoader { get; private set; }
+
+        public static void StartUp(IResourceLoader ressourceLoader, ISystemUtilities systemUtilities)
+        {
+            IWebserviceHost host = new WebserviceHost(ressourceLoader, systemUtilities);
+
+            WebServiceHostInstance = host;
+            SystemUtilitiesInstance = systemUtilities;
+            RessourceLoader = ressourceLoader;
+
+            host.StartListening();
+        }
+    }
+}
