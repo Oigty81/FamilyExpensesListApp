577f09c8600919a8f40c8438930a884e9f6b0335
Henry Voigt <oigty69@freenet.de>
2023-12-12 22:14:55 +0100

    added interface for system utilities classes

---
+++ Backend/Utilities/ISystemUtilities.cs
@@ -0,0 +1,17 @@
+﻿namespace Backend.Utilities
+{
+    using Backend.Types;
+
+    public interface ISystemUtilities
+    {
+        public OsSelectorOptions OsSelector { get; }
+
+        public string AppDirectory { get; }
+
+        public int GetBatteryState();
+
+        public void MinimizeApplication();
+
+        public void QuitApplication();
+    }
+}
