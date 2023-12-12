a303df862224765a9c6e113c2d4eed774b47adeb
Henry Voigt <oigty69@freenet.de>
2023-12-12 22:15:36 +0100

    added system-utilities-class for windows and android

---
+++ AppMain/SystemUtilities.cs
@@ -0,0 +1,63 @@
+﻿namespace AppMain
+{
+    using Backend;
+    using Backend.Utilities;
+    using Backend.Types;
+    using System.Diagnostics;
+
+#if WINDOWS
+    using Microsoft.UI;
+    using Microsoft.UI.Windowing;
+    using Windows.Graphics;
+#endif
+
+    public class SystemUtilities : ISystemUtilities
+    {
+        public SystemUtilities(OsSelectorOptions os, string appDirectory)
+        {
+            this.OsSelector = os;
+            this.AppDirectory = appDirectory;
+        }
+        public OsSelectorOptions OsSelector { get; }
+
+        public string AppDirectory { get; }
+
+        public int GetBatteryState()
+        {
+            return (int)(Battery.ChargeLevel * 100.0);
+        }
+
+        public void MinimizeApplication()
+        {
+#if WINDOWS
+            var mauiWindow = App.Current.Windows.First();
+            var nativeWindow = mauiWindow.Handler.PlatformView;
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+
+            PInvoke.User32.ShowWindow(windowHandle, PInvoke.User32.WindowShowStyle.SW_MINIMIZE);
+#endif
+#if ANDROID
+            var currentActivity = (MainActivity) Microsoft.Maui.ApplicationModel.Platform.CurrentActivity;
+            currentActivity.MoveTaskToBack(false);         
+#endif
+        }
+
+        public void QuitApplication()
+        {
+            try
+            {
+                Microsoft.Maui.ApplicationModel.MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Application.Current.Quit();
+                });
+
+                BackendMain.WebServiceHostInstance.StopListening();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("..." + e.Message);
+            }
+        }
+
+    }
+}
