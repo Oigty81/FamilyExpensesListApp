df53e155d33015e85fcb16d4b9e86fe4b90191e8
Henry Voigt <oigty69@freenet.de>
2023-12-14 23:29:19 +0100

    added CategoryController

---
+++ Backend/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿namespace Backend.Controllers
+{
+    using Backend;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/category")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getCategories")]
+        public async Task<ActionResult<bool>> GetCategories()
+        {
+            return true;
+        }
+
+        [HttpPost]
+        [Route("putCategory")]
+        public async Task<ActionResult<bool>> PutCategory()
+        {
+            return true;
+        }
+
+        [HttpPost]
+        [Route("putCategoryComposition")]
+        public async Task<ActionResult<bool>> PutCategoryComposition()
+        {
+            return true;
+        }
+    }
+}
