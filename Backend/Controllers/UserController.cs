89e777b10ecd2afaddc0ab2142b8dd29e8113c46
Henry Voigt <oigty69@freenet.de>
2023-12-14 23:29:39 +0100

    added UserController

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend.Controllers
+{
+    using Backend;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("auth")]
+        public async Task<ActionResult<Dictionary<string, string>>> Auth()
+        {
+            var response = new Dictionary<string, string>();
+            response["token"] = "AAAABBBBB";
+            return response;
+        }
+    }
+}
