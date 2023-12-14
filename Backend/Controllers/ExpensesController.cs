340cce11dbe08bfcab3d8129824b7a7b1d1f8c64
Henry Voigt <oigty69@freenet.de>
2023-12-14 23:30:04 +0100

    added ExpensesController

---
+++ Backend/Controllers/ExpensesController.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Controllers
+{
+    using Backend;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/expenses")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getExpensesPeriod")]
+        public async Task<ActionResult<bool>> ExpensesPeriod()
+        {
+            return true;
+        }
+
+        [HttpPost]
+        [Route("putExpenses")]
+        public async Task<ActionResult<bool>> PutExpenses()
+        {
+            return true;
+        }
+    }
+}
