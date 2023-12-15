6816287525eb6af5c699575129fa1b1d20c18f25
Henry Voigt <oigty69@freenet.de>
2023-12-15 01:34:43 +0100

    added Dto for token

---
+++ Backend/Dtos/TokenDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Dtos
+{
+    using Newtonsoft.Json;
+    using System;
+
+    [Serializable]
+    public class TokenDto
+    {
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get; set; }
+    }
+}
